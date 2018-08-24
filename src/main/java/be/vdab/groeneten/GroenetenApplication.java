ef31fddee7d186d78639f02892e6611257154b39
Stefaan <Stefaan@LOK111-04>
2018-08-24 14:10:49 +0200

    eerste commit

---
+++ src/main/java/be/vdab/groeneten/GroenetenApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.groeneten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroenetenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroenetenApplication.class, args);
+	}
+}
